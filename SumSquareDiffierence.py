750ae1490f5412861da401e531665a89a0ad0482
markrexon <68454892+markrexon@users.noreply.github.com>
2021-04-20 21:09:39 +0530

    Create SumSquareDiffierence.py
    answer is 25164150

---
+++ SumSquareDiffierence.py
@@ -0,0 +1,12 @@
+sq=0
+sum=0
+for i in range(1,101):
+    sum=sum+i**2
+    sq= (sq + i)
+result=(sq)**2
+final=result-sum
+print(result)
+print(sum)
+print("finally the answer for this question is:",final)
+
+
